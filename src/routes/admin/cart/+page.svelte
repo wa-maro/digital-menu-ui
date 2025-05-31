49ba384e2e89f5fb6c61b5bbfed7a6919ec72917
theomaro <theo.pombe@gmail.com>
2025-05-31 08:23:07 +0300

    add cart and order routes

--- src/routes/admin/cart/+page.svelte
+++ src/routes/admin/cart/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Cart</h1>
+<script lang="ts">
+</script>
+
+<div class="p-4">
+  <div class="flex justify-between items-center mb-4">
+    <h1 class="text-2xl font-bold text-[#044974]">Carts</h1>
+  </div>
+</div>
--- src/routes/admin/orders/+page.svelte
+++ src/routes/admin/orders/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Orders</h1>
+<script lang="ts">
+</script>
+
+<div class="p-4">
+  <div class="flex justify-between items-center mb-4">
+    <h1 class="text-2xl font-bold text-[#044974]">Orders</h1>
+  </div>
+</div>
