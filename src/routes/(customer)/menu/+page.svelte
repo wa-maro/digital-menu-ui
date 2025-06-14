958f795340ee7a98c501d6e5a514bb63bbd6bb2a
theomaro <theo.pombe@gmail.com>
2025-06-14 21:36:08 +0300

    update menu item card

--- src/routes/(customer)/menu/+page.svelte
+++ src/routes/(customer)/menu/+page.svelte
@@ -54,53 +54,52 @@
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
     {#each filteredItems as item}
       <div
-        class="bg-white flex flex-col justify-between border border-gray-200 rounded-xl shadow-sm hover:shadow-md transition"
+        class="bg-white border border-gray-200 rounded-xl shadow-sm hover:shadow-md transition"
       >
-        <div class="flex items-center flex-col gap-4 p-4">
-          <!-- Item Image -->
-          <img
-            src={item.imageURL || "https://via.placeholder.com"}
-            alt={item.name}
-            class="w-full h-44 object-cover rounded-lg flex-shrink-0"
-          />
+        <!-- Item Image -->
+        <img
+          src={item.imageURL || "https://via.placeholder.com"}
+          alt={item.name}
+          class="w-full h-44 object-cover rounded-lg flex-shrink-0"
+        />
 
-          <!-- Item Info -->
-          <div class="flex-1 space-y-2">
-            <h2>
-              <a href={`/menu/items/${item._id}`} class="text-gray-800 text-lg"
-                >{item.name}</a
-              >
-            </h2>
-
-            <div
-              class="flex items-center justify-between gap-2 text-xs text-gray-500"
+        <!-- Item Info -->
+        <div class="flex-1 space-y-2 p-4">
+          <h2>
+            <a href={`/menu/items/${item._id}`} class="text-gray-800 text-lg"
+              >{item.name}</a
             >
-              {#if item.available}
-                <span class="text-green-600 font-medium">Available</span>
-              {:else}
-                <span class="text-red-500 font-medium">Unavailable</span>
-              {/if}
-            </div>
+          </h2>
 
-            <p class="text-sm text-gray-500 line-clamp-2">
-              {item.description}
-            </p>
+          <div
+            class="flex items-center justify-between gap-2 text-xs text-gray-500"
+          >
+            {#if item.available}
+              <span class="text-green-600 font-medium">Available</span>
+            {:else}
+              <span class="text-red-500 font-medium">Unavailable</span>
+            {/if}
 
             <div>
               <span class="bg-gray-100 px-2 py-0.5 rounded-full"
                 >{item.category.name}</span
               >
             </div>
+          </div>
 
-            <div
-              class="flex items-center justify-between gap-2 text-xs text-gray-500"
+          <p class="text-sm text-gray-500 line-clamp-2">
+            {item.description}
+          </p>
+
+          <div
+            class="flex items-center justify-between gap-2 text-xs text-gray-500"
+          >
+            <span class="text-sm font-medium text-[#065B8C]"
+              >TZS {item.price.toFixed(2)}</span
             >
-              <span class="text-sm font-medium text-[#065B8C]"
-                >TZS {item.price.toFixed(2)}</span
-              >
-            </div>
           </div>
         </div>
+
         <!-- Add to Cart -->
         {#if item.available}
           <div class="p-4"><AddToCart {item} /></div>
