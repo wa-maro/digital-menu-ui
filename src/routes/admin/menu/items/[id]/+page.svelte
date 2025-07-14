842ca7775d824ccc944f87c53c70c260ad540e48
theomaro <theo.pombe@gmail.com>
2025-07-14 12:34:35 +0300

    delete item from details page

--- src/routes/admin/menu/items/[id]/+page.svelte
+++ src/routes/admin/menu/items/[id]/+page.svelte
@@ -61,4 +61,29 @@
       </p>
     </div>
   </div>
+
+  <form action="./?/delete" method="post" class="py-8">
+    <input type="hidden" name="_id" bind:value={item._id} />
+    <button
+      type="submit"
+      class="flex items-center gap-2 px-4 py-2 bg-red-600 text-white rounded-2xl shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-200"
+      aria-label="Delete"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-5 h-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 7L5 7M10 11V17M14 11V17M5 7L6 19C6 20.1046 6.89543 21 8 21H16C17.1046 21 18 20.1046 18 19L19 7M9 7V4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4V7"
+        />
+      </svg>
+      Delete Item
+    </button>
+  </form>
 </div>
