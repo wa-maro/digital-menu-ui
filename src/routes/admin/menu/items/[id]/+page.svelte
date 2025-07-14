dd0b2828acf9809c83f21efc0e3d88e94ad18d8c
theomaro <theo.pombe@gmail.com>
2025-07-14 15:49:31 +0300

    restyle delete/edit buttons

--- src/routes/admin/menu/items/[id]/+page.svelte
+++ src/routes/admin/menu/items/[id]/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="max-w-4xl mx-auto space-y-6">
   <!-- Back Button -->
-  <div class="pt-6">
+  <div class="pt-3">
     <a href="/admin/menu/items" class="text-sm text-gray-600 hover:underline"
       >← Back to menu</a
     >
@@ -14,12 +14,6 @@
   <!-- Header -->
   <div class="flex justify-between items-center w-3/4">
     <h1 class="text-2xl font-bold text-gray-800">{item.name}</h1>
-    <a
-      href={`/admin/menu/items/${item._id}/update`}
-      class="text-blue-600 hover:underline"
-    >
-      Edit
-    </a>
   </div>
 
   <!-- Image -->
@@ -62,28 +56,54 @@
     </div>
   </div>
 
-  <form action="./?/delete" method="post" class="py-8">
-    <input type="hidden" name="_id" bind:value={item._id} />
-    <button
-      type="submit"
-      class="flex items-center gap-2 px-4 py-2 bg-red-600 text-white rounded-2xl shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-200"
-      aria-label="Delete"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="w-5 h-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
+  <div class="flex gap-x-8 py-8">
+    <div>
+      <a
+        href={`/admin/menu/items/${item._id}/update`}
+        class="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-2xl shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+        aria-label="edit"
       >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M19 7L5 7M10 11V17M14 11V17M5 7L6 19C6 20.1046 6.89543 21 8 21H16C17.1046 21 18 20.1046 18 19L19 7M9 7V4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4V7"
-        />
-      </svg>
-      Delete Item
-    </button>
-  </form>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-5 h-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M11 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2v-5M18.5 2.5a2.121 2.121 0 113 3L12 15l-4 1 1-4 9.5-9.5z"
+          />
+        </svg>
+        Edit Item
+      </a>
+    </div>
+
+    <form action="./?/delete" method="post">
+      <input type="hidden" name="_id" bind:value={item._id} />
+      <button
+        type="submit"
+        class="flex items-center gap-2 px-4 py-2 bg-red-600 text-white rounded-2xl shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-200"
+        aria-label="Delete"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-5 h-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 7L5 7M10 11V17M14 11V17M5 7L6 19C6 20.1046 6.89543 21 8 21H16C17.1046 21 18 20.1046 18 19L19 7M9 7V4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4V7"
+          />
+        </svg>
+        Delete Item
+      </button>
+    </form>
+  </div>
 </div>
