d2e8ddc70f58633e7e24982260d61309dcce3224
theomaro <theo.pombe@gmail.com>
2025-07-21 10:55:07 +0300

    configure carbon charts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	ssr: {
+		noExternal: process.env.NODE_ENV === 'production' ? ['@carbon/charts'] : []
+	}
 });
